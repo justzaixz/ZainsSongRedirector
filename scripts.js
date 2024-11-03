febda7c83779123f648ce251bdcede7ba8d23812
justzaixz <165422157+justzaixz@users.noreply.github.com>
2024-11-03 14:37:46 +0400

    else if

--- scripts.js
+++ scripts.js
@@ -61,28 +61,27 @@ function generateLink() {
 }
 
 function gotolikes() {
-   
-    console.log("Go to likes button clicked")
-    let streamingWebsite = document.getElementById("streamingWebsites").value;
-    if (streamingWebsite === "Please Select a Streaming Website") {
-        console.log('User Did not select anything (STREAMINGWEBSITE )');
-        alert("Please Select A Streaming Website.");
-    } else {
-        console.log('User Selected a Streaming Website')
-        if (streamingWebsite === "https://play.anghami.com/search/") {
-            console.log('User Selected Anghami for Likes')
-            let fullUrl = "https://play.anghami.com/likes";
-            window.location.href = fullUrl;
-            console.log(fullUrl);
+    console.log("Go to likes button clicked");
+    const streamingWebsite = document.getElementById("streamingWebsites").value;
     
-        }
-        else {
-            let fullUrl = "https://open.spotify.com/collection/tracks";
-            window.location.href = fullUrl;
-            console.log(fullUrl);
-        }
+    if (streamingWebsite === "Please Select a Streaming Website") {
+        console.log('User did not select a streaming website');
+        alert("Please Select A Streaming Website.");
+    } else if (streamingWebsite === "https://play.anghami.com/search/") {
+        console.log('User selected Anghami for Likes');
+        const anghamiLikesUrl = "https://play.anghami.com/likes";
+        window.location.href = anghamiLikesUrl;
+        console.log(anghamiLikesUrl);
+    } else if (streamingWebsite === "https://open.spotify.com/search/") {
+        console.log('User selected Spotify for Likes');
+        const spotifyLikesUrl = "https://open.spotify.com/collection/tracks";
+        window.location.href = spotifyLikesUrl;
+        console.log(spotifyLikesUrl);
+    } else {
+        console.log("No valid streaming service selected.");
     }
 }
+
 
 
 document.getElementById("songNameInput").addEventListener("click", function() {
